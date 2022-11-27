fdcd830b03f8cd17cea73e95c52779c0bccb625e
Rogerio Almeida <rogerio.mattos@stoom.com.br>
2022-11-27 16:27:17 -0300

    Ajuste no base url

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseApi = axios.create({
-  baseURL: 'https://api.nytimes.com/svc/topstories/v2/',
+  baseURL: 'https://api.nytimes.com/svc/',
   params: {
     'api-key':process.env.API_KEY
   }
--- src/services/feedService.ts
+++ src/services/feedService.ts
@@ -3,6 +3,6 @@ import { Article } from '@types/Article';
 import { Response } from '@types/Response';
 
 export const feedService = ({
-  getHome: async () => (await baseApi.get<Response<Article>>('/home.json')).data
+  getHome: async () => (await baseApi.get<Response<Article>>('/topstories/v2/home.json')).data
 })
 
