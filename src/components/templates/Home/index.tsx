e29c7501cbc688c0f09f71b15af33ba4be060f23
Rogerio Almeida <rogerio.mattos@stoom.com.br>
2022-11-27 14:59:24 -0300

    Adicionando noticias em destaque no template

--- src/components/templates/Home/index.tsx
+++ src/components/templates/Home/index.tsx
@@ -1,9 +1,10 @@
+import { useMemo } from 'react';
 import Header from '@components/organisms/Header';
 import Feed from '@components/organisms/Feed';
 import { Article } from '@types/Article';
 import { Response } from '@types/Response';
-import ArticleCard from '@components/molecules/ArticleCard';
-
+import EmphasisArticles from '@components/organisms/EmphasisArticles';
+import { useIsMobile } from '@hooks/useIsMobile';
 
 type HomeProps = {
   responseArticles: Response<Article>;
@@ -12,12 +13,18 @@ type HomeProps = {
 const Home = ({
   responseArticles
 }: HomeProps) => {
+  const isMobile = useIsMobile();
+
+  const recentlyThreeArticles = useMemo(() => responseArticles.results.filter((_, index)=> index <= 2), [responseArticles]);
+  const lastArticles = useMemo(() => responseArticles.results.filter((_, index)=> isMobile || index > 2), [responseArticles, isMobile]);
+
   return(
     <div>
       <Header/>
       <main>
-        {responseArticles.results[0] && <ArticleCard article={responseArticles.results[0]}/>}
-        <Feed articles={responseArticles.results}/>
+        {!isMobile &&
+        <EmphasisArticles articleList={recentlyThreeArticles}/>}
+        <Feed articles={lastArticles}/>
       </main>
     </div>
   );
